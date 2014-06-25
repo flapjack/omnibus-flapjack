c693cc7d5323cce22069b5f040cd678715d2b45a
Jesse Reynolds <jesse@va.com.au>
2014-06-26 00:00:42 +0930

    add a cache-warmer project to have omnibus install flapjack's dependencies only

---
+++ config/projects/flapjack-dependencies.rb
@@ -0,0 +1,28 @@
+
+name          "flapjack-dependencies"
+friendly_name "Flapjack's Dependencies"
+maintainer    "Lindsay Holmwood, Jesse Reynolds, Ali Graham"
+homepage      "http://flapjack.io"
+
+install_path   "/opt/flapjack-dependencies"
+
+build_version  "#{Time.now.strftime('%Y%m%d%H%M%S')}"
+build_iteration 1
+
+# creates required build directories
+dependency "preparation"
+
+# flapjack dependencies/components
+# dependency "somedep"
+
+# version manifest file
+dependency "version-manifest"
+
+exclude "\.git*"
+exclude "bundler\/git"
+
+dependency "ruby"
+dependency "redis"
+dependency "yajl"
+dependency "zlib"
+dependency "nokogiri"
