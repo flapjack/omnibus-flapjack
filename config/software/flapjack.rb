9fa50923362a1abd7b2df4de2b01e5ff5ca6d428
Jesse Reynolds <jesse@va.com.au>
2014-06-26 22:13:55 +0930

    try forgetting bundler and just installing flapjack from rubygems.org

--- config/software/flapjack.rb
+++ config/software/flapjack.rb
@@ -8,32 +8,32 @@ dependency "rubygems"
 dependency "bundler"
 dependency "nokogiri"
 
-source :git => "https://github.com/flapjack/flapjack"
+#source :git => "https://github.com/flapjack/flapjack"
 
 relative_path "flapjack"
 
 build do
   # Install all dependencies
-  command "rm Gemfile.lock"
+  #command "rm Gemfile.lock"
   #command "PATH=#{install_dir}/embedded/bin:${PATH} ; export PATH"
 
   #bundle "install --path=#{install_dir}/embedded/service/gem"
-  command "PATH=#{install_dir}/embedded/bin:$PATH" +
-          " #{install_dir}/embedded/bin/bundle install" +
-          " --path=#{install_dir}/embedded/service/gem"
+  #command "PATH=#{install_dir}/embedded/bin:$PATH" +
+  #        " #{install_dir}/embedded/bin/bundle install" +
+  #        " --path=#{install_dir}/embedded/service/gem"
 
   # Build + install the gem
   #bundle "exec rake build"
-  command "PATH=#{install_dir}/embedded/bin:$PATH" +
-          " #{install_dir}/embedded/bin/bundle install" +
-          " --path=#{install_dir}/embedded/service/gem"
+  #command "PATH=#{install_dir}/embedded/bin:$PATH" +
+  #        " #{install_dir}/embedded/bin/bundle install" +
+  #        " --path=#{install_dir}/embedded/service/gem"
 
   #gem [ "install pkg/flapjack*.gem",
   #      "--bindir #{install_dir}/bin",
   #      "--no-rdoc --no-ri" ].join(" ")
 
   command "PATH=#{install_dir}/embedded/bin:$PATH" +
-          " #{install_dir}/embedded/bin/gem install pkg/flapjack*.gem" +
+          " #{install_dir}/embedded/bin/gem install flapjack --version #{ENV['FLAPJACK_BUILD_TAG']}" +
           " --bindir #{install_dir}/bin" +
           " --no-rdoc --no-ri"
 end
