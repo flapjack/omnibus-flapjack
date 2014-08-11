f987410f774198b3269fc26ab86066f5cd9c7f1c
Sarah Kowalik <sarah.kowalik@bulletproof.net>
2014-08-11 18:05:07 +1000

    Handle command line arguments

--- run.sh
+++ run.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 set -e
 
-date=$(date +%Y%m%d%H%M%S)
-type=${type:-experimental}
-FLAPJACK_BUILD_REF=${FLAPJACK_BUILD_REF:-master}
+args=("$@")
+if [ $# -ne 2 ]
+then
+  echo "Usage: `basename $0` build_type build_ref"
+  echo "eg. `basename $0` experimental master"
+  exit 2
+fi
+
+DATE=$(date +%Y%m%d%H%M%S)
+BUILD_TYPE=$1
+FLAPJACK_BUILD_REF=$2
+# FIXME: Find this from lib/flapjack/version.rb
+FLAPJACK_BUILD_TAG='1.0.0~rc3'
 
 docker run -i -t -e "FLAPJACK_BUILD_REF=${FLAPJACK_BUILD_REF}" \
--e "FLAPJACK_PACKAGE_VERSION=${FLAPJACK_BUILD_TAG}-${date}-${FLAPJACK_BUILD_REF}" \
+-e "FLAPJACK_PACKAGE_VERSION=${FLAPJACK_BUILD_TAG}~${DATE}-${FLAPJACK_BUILD_REF}" \
 flapjack/omnibus-ubuntu bash -c \
 "cd omnibus-flapjack ; \
 git pull ; \
 bundle install --binstubs ; \
-bin/omnibus build --log-level=info flapjack ; \
-bash"
+bin/omnibus build --log-level=info flapjack"
 
 container_id=`docker ps -l -q`
 docker cp ${container_id}:/omnibus-flapjack/pkg .
 docker rm ${container_id}
+
+exit 0
+
+# Check if awscli exists
+if not hash aws 2>/dev/null; then
+  apt-get install -y awscli
+fi
 
 # Check if aptly exists
 if not hash aptly 2>/dev/null; then
@@ -50,26 +66,26 @@ if not hash aptly 2>/dev/null; then
 }
 EOF
   fi
-
 fi
 # End aptly installation
 
+# Put packages into aptly repo, sync with S3
 mkdir -p aptly
 aws s3 sync s3://packages.flapjack.io/aptly aptly --acl private --region us-east-1
 
 # Create the repo if it doesn't exist
-if ! aptly -config=aptly.conf repo show flapjack-${type} 2>/dev/null ; then
-  aptly -config=aptly.conf repo create --distribution ${type} flapjack-${type}
+if ! aptly -config=aptly.conf repo show flapjack-${BUILD_TYPE} 2>/dev/null ; then
+  aptly -config=aptly.conf repo create --distribution ${BUILD_TYPE} flapjack-${BUILD_TYPE}
 fi
 
 
-if ! aptly -config=aptly.conf repo add flapjack-${type} pkg/flapjack_${FLAPJACK_BUILD_TAG}-${date}-${FLAPJACK_BUILD_REF}.deb ; then
+if ! aptly -config=aptly.conf repo add flapjack-${BUILD_TYPE} pkg/flapjack_${FLAPJACK_BUILD_TAG}-${date}-${FLAPJACK_BUILD_REF}.deb ; then
   echo "Error adding deb to repostory" ; exit $? ;
 fi
 
 # Try updating the published repository, otherwise do the first publish
-if ! aptly -config=aptly.conf -gpg-key="803709B6" publish update ${type} ; then
-  aptly -config=aptly.conf -gpg-key="803709B6" publish repo flapjack-${type}
+if ! aptly -config=aptly.conf -gpg-key="803709B6" publish update ${BUILD_TYPE} ; then
+  aptly -config=aptly.conf -gpg-key="803709B6" publish repo flapjack-${BUILD_TYPE}
 fi
 
 aws s3 sync aptly s3://packages.flapjack.io/aptly --acl private --region us-east-1
