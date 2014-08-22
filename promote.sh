255fd3b779748bbafb7a3c1d1108e50d0405cfe8
Jesse Reynolds <jesse@va.com.au>
2014-08-22 10:44:28 +1000

    get promote.sh working with bash

--- promote.sh
+++ promote.sh
@@ -16,34 +16,44 @@ DISTRO_RELEASE=$(echo ${VERSION} | cut -d '-' -f 3)
 MAIN_VERSION=$(echo ${VERSION} | cut -d "~" -f 1)-${DISTRO_RELEASE}
 FLAPJACK_MAJOR_VERSION=$(echo ${VERSION} | cut -d . -f 1,2)
 
-echo "Copying candidate package for main to s3"
-if [ ! aws s3 cp s3://packages.flapjack.io/candidates/${FILENAME} .--acl public-read --region us-east-1 ]
-  echo "Couldn't find package at pkg/${FILENAME}"
-  exit 3
+if [ ! -e "pkg/${FILENAME}" ] ; then
+  echo "Copying candidate package from s3"
+  aws s3 cp s3://packages.flapjack.io/candidates/${FILENAME} . --acl public-read --region us-east-1
+  retval=$?
+  if [ ! "$retval" -eq "0" ] ; then
+    echo "Couldn't upload package from pkg/${FILENAME}"
+    exit $retval
+  fi
 fi
 
 MAIN_FILENAME=${VERSION_ARRAY[1]}_${MAIN_VERSION}_${VERSION_ARRAY[3]}
 cp ${FILENAME} ${MAIN_FILENAME}
 echo "New package is at ${MAIN_FILENAME}"
 
-echo "Putting packages into aptly repo, syncing with S3"
+echo "Retrieving aptly repo from S3"
 mkdir -p aptly
 aws s3 sync s3://packages.flapjack.io/aptly aptly --delete --acl public-read --region us-east-1
 
 echo "Adding pkg/${FILENAME} to the flapjack-${FLAPJACK_MAJOR_VERSION}-${DISTRO_RELEASE}-main repo"
-if ! aptly -config=aptly.conf repo add flapjack-${FLAPJACK_MAJOR_VERSION}-${DISTRO_RELEASE}-main ${MAIN_FILENAME} ; then
-  echo "Error adding deb to repostory" ; exit $?
+aptly -config=aptly.conf repo add flapjack-${FLAPJACK_MAJOR_VERSION}-${DISTRO_RELEASE}-main ${MAIN_FILENAME}
+retval=$?
+if [ ! "$retval" -eq "0" ] ; then
+  echo "Error adding deb to repostory, aptly returned ${retval}"
+  exit $retval
 fi
 
-echo "Repository already published, attempting an update"
-# Aptly checks the inode number to determine if packages are the same.  As we sync from S3, our inode numbers change, so identical packages are deemed different.
+echo "Updating the already published repository ${DISTRO_RELEASE}"
+# Aptly checks the inode number to determine if packages are the same.  As we sync from S3, our inode numbers change, so identical packages are deemed different unless we use -force-overwrite
 aptly -config=aptly.conf -gpg-key="803709B6" -force-overwrite=true publish update ${DISTRO_RELEASE} ${FLAPJACK_MAJOR_VERSION}
 
 echo "Creating directory index files for published packages"
 cd aptly/public
-if ! ../../create_directory_listings . ; then
-  echo "Directory indexes failed to create"
+../../create_directory_listings .
+if [ ! "$retval" -eq "0" ] ; then
+  echo "Directory indexes failed to be created"
+  exit $retval
 fi
+
 cd -
 
 echo "Syncing the aptly db up to S3"
