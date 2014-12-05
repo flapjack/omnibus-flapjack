d93166bd857d107446ed6ccdd0be3c9979d5bc85
Sarah Kowalik <sarah.kowalik@bulletproof.net>
2014-12-05 12:20:44 +1100

    Livestream everywhere

--- lib/omnibus-flapjack/publish.rb
+++ lib/omnibus-flapjack/publish.rb
@@ -15,7 +15,7 @@ module OmnibusFlapjack
             return
           end
           puts "Creating directory index files for published packages"
-          indexes = Mixlib::ShellOut.new("#{list_script} .")
+          indexes = Mixlib::ShellOut.new("#{list_script} .", :live_stream => $stdout)
           if indexes.run_command.error?
             puts "Warning: Directory indexes failed to be created"
             puts indexes.inspect
@@ -29,7 +29,7 @@ module OmnibusFlapjack
           obtained_lock = false
           (1..360).each do |i|
             if Mixlib::ShellOut.new("aws s3 cp s3://packages.flapjack.io/#{lockfile} #{lockfile}" +
-              "--acl public-read --region us-east-1").run_command.error?
+              "--acl public-read --region us-east-1", :live_stream => $stdout).run_command.error?
               obtained_lock = true
               break
             end
@@ -45,14 +45,14 @@ module OmnibusFlapjack
         duration_string = ChronicDuration.output(get_lock_duration.round(0), :format => :short)
 
         puts "Took #{duration_string} to get lockfile.  Starting package upload"
-        Mixlib::ShellOut.new("touch #{lockfile}").run_command.error!
+        Mixlib::ShellOut.new("touch #{lockfile}", :live_stream => $stdout).run_command.error!
         Mixlib::ShellOut.new("aws s3 cp #{lockfile} s3://packages.flapjack.io/#{lockfile} --acl public-read " +
-                             "--region us-east-1").run_command.error!
+                             "--region us-east-1", :live_stream => $stdout).run_command.error!
       end
 
       def release_lock(lockfile)
         puts "Removing package upload lockfile"
-        if Mixlib::ShellOut.new("aws s3 rm s3://packages.flapjack.io/#{lockfile} --region us-east-1").run_command.error?
+        if Mixlib::ShellOut.new("aws s3 rm s3://packages.flapjack.io/#{lockfile} --region us-east-1", :live_stream => $stdout).run_command.error?
           puts "Failed to remove lockfile - please remove s3://packages.flapjack.io/#{lockfile} manually"
           exit 5
         end
@@ -63,7 +63,7 @@ module OmnibusFlapjack
 
         puts "Syncing down #{remote_dir} to #{local_dir}"
         Mixlib::ShellOut.new("aws s3 sync #{remote_dir} #{local_dir} --delete " +
-                             "--acl public-read --region us-east-1").run_command.error!
+                             "--acl public-read --region us-east-1", :live_stream => $stdout).run_command.error!
       end
 
       def sync_packages_to_remote(local_dir, remote_dir)
@@ -73,13 +73,13 @@ module OmnibusFlapjack
         end
         puts "Syncing #{local_dir} up to #{remote_dir}"
         Mixlib::ShellOut.new("aws s3 sync #{local_dir} #{remote_dir} " +
-                             "--delete --acl public-read --region us-east-1").run_command.error!
+                             "--delete --acl public-read --region us-east-1", :live_stream => $stdout).run_command.error!
       end
 
       def add_to_deb_repo(pkg, component = 'experimental')
         puts "Checking aptly db for errors"
-        Mixlib::ShellOut.new("aptly -config aptly.conf db recover").run_command.error!
-        Mixlib::ShellOut.new("aptly -config aptly.conf db cleanup").run_command.error!
+        Mixlib::ShellOut.new("aptly -config aptly.conf db recover", :live_stream => $stdout).run_command.error!
+        Mixlib::ShellOut.new("aptly -config aptly.conf db cleanup", :live_stream => $stdout).run_command.error!
 
         puts "Creating all components for the distro release if they don't exist"
 
@@ -87,11 +87,11 @@ module OmnibusFlapjack
 
         valid_components.each do |comp|
           if Mixlib::ShellOut.new("aptly -config=aptly.conf repo show " +
-                                  "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{comp}"
+                                  "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{comp}", :live_stream => $stdout
                                   ).run_command.error?
             Mixlib::ShellOut.new("aptly -config=aptly.conf repo create -distribution #{pkg.distro_release} " +
                                  "-architectures='i386,amd64' -component=#{comp} " +
-                                 "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{comp}"
+                                 "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{comp}", :live_stream => $stdout
                                  ).run_command.error!
           end
         end
@@ -111,7 +111,7 @@ module OmnibusFlapjack
              "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{component} repo"
         Mixlib::ShellOut.new("aptly -config=aptly.conf repo add " +
                              "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{component} " +
-                             "pkg/#{source_file}").run_command.error!
+                             "pkg/#{source_file}", :live_stream => $stdout).run_command.error!
 
         puts "Attempting the first publish for all components of the major version " +
              "of the given distro release"
@@ -121,12 +121,12 @@ module OmnibusFlapjack
           publish_cmd += "flapjack-#{pkg.major_version}-#{pkg.distro_release}-#{comp} "
         end
         publish_cmd += " #{pkg.major_version}"
-        if Mixlib::ShellOut.new(publish_cmd).run_command.error?
+        if Mixlib::ShellOut.new(publish_cmd, :live_stream => $stdout).run_command.error?
           puts "Repository already published, attempting an update"
           # Aptly checks the inode number to determine if packages are the same.
           # As we sync from S3, our inode numbers change, so identical packages are deemed different.
           Mixlib::ShellOut.new('aptly -config=aptly.conf -gpg-key="803709B6" -force-overwrite=true ' +
-                               "publish update #{pkg.distro_release} #{pkg.major_version}").run_command.error!
+                               "publish update #{pkg.distro_release} #{pkg.major_version}", :live_stream => $stdout).run_command.error!
         end
       end
 
@@ -151,7 +151,7 @@ module OmnibusFlapjack
                   puts "New RPM repo: #{local_dir}"
                   FileUtils.mkdir_p local_dir
                   Dir.chdir(local_dir) do
-                    createrepo_cmd = Mixlib::ShellOut.new('createrepo .')
+                    createrepo_cmd = Mixlib::ShellOut.new('createrepo .', :live_stream => $stdout)
                     unless createrepo_cmd.run_command
                       puts "Error running 'createrepo .', exit status is #{createrepo_cmd.exitstatus}"
                       puts "PWD:    #{FileUtils.pwd}"
@@ -189,11 +189,11 @@ module OmnibusFlapjack
         raise "Source package file doesn't exist [#{source_file}]" unless File.file?("pkg/#{source_file}")
 
         puts "Adding pkg/#{source_file} to the #{name.join('-')} repo"
-        Mixlib::ShellOut.new("cp pkg/#{source_file} #{File.join(base_dir, *name)}/.").run_command.error!
+        Mixlib::ShellOut.new("cp pkg/#{source_file} #{File.join(base_dir, *name)}/.", :live_stream => $stdout).run_command.error!
 
         puts "Updating #{name.join('-')} repo"
         Dir.chdir(File.join(base_dir, *name)) do
-          Mixlib::ShellOut.new('createrepo .').run_command.error!
+          Mixlib::ShellOut.new('createrepo .', :live_stream => $stdout).run_command.error!
         end
       end
     end # class << self
