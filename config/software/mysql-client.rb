c68e5ca15bc9fc71f7075c7aaa7e3911161136f3
Jesse Reynolds <jesse@va.com.au>
2015-02-11 14:03:58 +1030

    s/max_build_jobs/workers/ for omnibus 4 happiness

--- config/software/mysql-client.rb
+++ config/software/mysql-client.rb
@@ -58,7 +58,7 @@ build do
            ].join(" "), :env => env
 
   %w{libmysql client include}.each do |target|
-    command "make -j #{max_build_jobs} install", :env => env, :cwd => "#{project_dir}/#{target}"
+    command "make -j #{workers} install", :env => env, :cwd => "#{project_dir}/#{target}"
   end
 end
 
