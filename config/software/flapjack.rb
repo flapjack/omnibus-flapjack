182f62358efc0961676883a61be17c6260152b38
Ali Graham <ali.graham@bulletproof.net>
2015-10-08 09:26:37 +1030

    change gem dir discovery in flapjack build def

--- config/software/flapjack.rb
+++ config/software/flapjack.rb
@@ -22,45 +22,39 @@ omnibus_flapjack_path = Dir.pwd
 
 build do
   command "if [ ! -d flapjack_source ] ; then git clone https://github.com/flapjack/flapjack.git flapjack_source ; fi"
-  command "cd flapjack_source && " +
-          "git checkout master && " +
-          "git pull && " +
-          "git checkout #{build_ref} && " +
+  command "cd flapjack_source && " \
+          "git checkout master && " \
+          "git pull && " \
+          "git checkout #{build_ref} && " \
           "/opt/flapjack/embedded/bin/gem build flapjack.gemspec"
   gem [ "install /var/cache/omnibus/src/flapjack/flapjack_source/flapjack*gem",
         "--bindir #{install_dir}/bin",
         "--no-rdoc --no-ri" ].join(" ")
 
-  command "export gem_home=/" +
-          "`/opt/flapjack/embedded/bin/gem list --all --details flapjack | " +
-          "  grep 'Installed at' | sed 's/^.* \\///'` ; " +
-          "echo \"gem_home: ${gem_home}\" ; " +
-          "export installed_gem=`ls -dtr ${gem_home}/gems/flapjack* | tail -1` ; " +
+  command "export gem_home=\"`/opt/flapjack/embedded/bin/gem environment gemdir`\" ; " \
+          "echo \"gem_home: ${gem_home}\" ; " \
+          "export installed_gem=\"`ls -dtr ${gem_home}/gems/flapjack* | tail -1`\" ; " \
           "cd ${installed_gem}"
   if compile_go_components
-    command "export gem_home=/" +
-      "`/opt/flapjack/embedded/bin/gem list --all --details flapjack | " +
-      "  grep 'Installed at' | sed 's/^.* \\///'` ; " +
-      "echo \"gem_home: ${gem_home}\" ; " +
-      "export installed_gem=`ls -dtr ${gem_home}/gems/flapjack* | tail -1` ; " +
-      "cd ${installed_gem} && " +
-      "./build.sh"
+    command "export gem_home=\"`/opt/flapjack/embedded/bin/gem environment gemdir`\" ; " \
+            "echo \"gem_home: ${gem_home}\" ; " \
+            "export installed_gem=\"`ls -dtr ${gem_home}/gems/flapjack* | tail -1`\" ; " \
+            "cd ${installed_gem} && " \
+            "./build.sh"
   end
 
   # Build flapjackfeeder, as per https://github.com/flapjack/flapjackfeeder
-  command "export gem_home=/" +
-    "`/opt/flapjack/embedded/bin/gem list --all --details flapjack | " +
-    "  grep 'Installed at' | sed 's/^.* \\///'` ; " +
-    "echo \"gem_home: ${gem_home}\" ; " +
-    "export installed_gem=`ls -dtr ${gem_home}/gems/flapjack* | tail -1` ; " +
-    "cd ${installed_gem} && " +
-    "if [ ! -d flapjackfeeder ] ; then git clone https://github.com/flapjack/flapjackfeeder.git flapjackfeeder ; fi && " +
-    "cd flapjackfeeder && " +
-    "make && " +
-    "cd .. && " +
-    "cp flapjackfeeder/flapjackfeeder3-*.o flapjackfeeder3.o && " +
-    "cp flapjackfeeder/flapjackfeeder4-*.o flapjackfeeder4.o && " +
-    "rm -r flapjackfeeder"
+  command "export gem_home=\"`/opt/flapjack/embedded/bin/gem environment gemdir`\" ; " \
+          "echo \"gem_home: ${gem_home}\" ; " \
+          "export installed_gem=\"`ls -dtr ${gem_home}/gems/flapjack* | tail -1`\" ; " \
+          "cd ${installed_gem} && " \
+          "if [ ! -d flapjackfeeder ] ; then git clone https://github.com/flapjack/flapjackfeeder.git flapjackfeeder ; fi && " \
+          "cd flapjackfeeder && " \
+          "make && " \
+          "cd .. && " \
+          "cp flapjackfeeder/flapjackfeeder3-*.o flapjackfeeder3.o && " \
+          "cp flapjackfeeder/flapjackfeeder4-*.o flapjackfeeder4.o && " \
+          "rm -r flapjackfeeder"
 
     command "cp #{omnibus_flapjack_path}/dist/etc/init.d/flapjack* #{etc_path}/init.d/"
     command "cp #{omnibus_flapjack_path}/dist/etc/init.d/flapper* #{etc_path}/init.d/"
